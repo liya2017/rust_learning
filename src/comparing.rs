cc7d49d9bbcb28af9026a2e99bd1813edae53153
liya2017 <Liya@cryptape.com>
2021-11-16 17:03:58 +0800

    compariomg the guess to the secret number

---
+++ src/comparing.rs
@@ -0,0 +1,12 @@
+use std::io;
+fn main() { 
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    let mut guess = String::new(); //variables are immutable by default in rust, mut crate a nutable var
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
